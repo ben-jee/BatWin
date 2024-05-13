ef55d03a962118b743246ad701660078112f7228
ben-jee <75759861+ben-jee@users.noreply.github.com>
2024-05-13 13:50:41 +0100

    New files and bug fixes
    Added estimated life remaining.
    Disabled fullscreen on double-click.
    Added colour coded form background.
    Made transparent.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,37 +30,62 @@
         {
             this.labelCurrentUser = new System.Windows.Forms.Label();
             this.labelBatteryPercent = new System.Windows.Forms.Label();
+            this.labelBatteryLifeRemaining = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelCurrentUser
             // 
             this.labelCurrentUser.AutoSize = true;
-            this.labelCurrentUser.Location = new System.Drawing.Point(192, 30);
+            this.labelCurrentUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCurrentUser.Location = new System.Drawing.Point(12, 14);
             this.labelCurrentUser.Name = "labelCurrentUser";
-            this.labelCurrentUser.Size = new System.Drawing.Size(35, 13);
+            this.labelCurrentUser.Size = new System.Drawing.Size(66, 24);
             this.labelCurrentUser.TabIndex = 0;
             this.labelCurrentUser.Text = "label1";
+            this.labelCurrentUser.Click += new System.EventHandler(this.labelCurrentUser_Click);
             // 
             // labelBatteryPercent
             // 
             this.labelBatteryPercent.AutoSize = true;
-            this.labelBatteryPercent.Location = new System.Drawing.Point(192, 148);
+            this.labelBatteryPercent.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBatteryPercent.Location = new System.Drawing.Point(12, 38);
             this.labelBatteryPercent.Name = "labelBatteryPercent";
-            this.labelBatteryPercent.Size = new System.Drawing.Size(35, 13);
+            this.labelBatteryPercent.Size = new System.Drawing.Size(60, 24);
             this.labelBatteryPercent.TabIndex = 1;
             this.labelBatteryPercent.Text = "label2";
+            this.labelBatteryPercent.Click += new System.EventHandler(this.labelBatteryPercent_Click);
+            // 
+            // labelBatteryLifeRemaining
+            // 
+            this.labelBatteryLifeRemaining.AutoSize = true;
+            this.labelBatteryLifeRemaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBatteryLifeRemaining.Location = new System.Drawing.Point(12, 62);
+            this.labelBatteryLifeRemaining.Name = "labelBatteryLifeRemaining";
+            this.labelBatteryLifeRemaining.Size = new System.Drawing.Size(60, 24);
+            this.labelBatteryLifeRemaining.TabIndex = 2;
+            this.labelBatteryLifeRemaining.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(441, 208);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(235, 95);
+            this.ControlBox = false;
+            this.Controls.Add(this.labelBatteryLifeRemaining);
             this.Controls.Add(this.labelBatteryPercent);
             this.Controls.Add(this.labelCurrentUser);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximumSize = new System.Drawing.Size(235, 95);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(235, 95);
             this.Name = "Form1";
+            this.Opacity = 0.75D;
             this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,6 +95,7 @@
 
         private System.Windows.Forms.Label labelCurrentUser;
         private System.Windows.Forms.Label labelBatteryPercent;
+        private System.Windows.Forms.Label labelBatteryLifeRemaining;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,55 +1,137 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Exam_Window
 {
     public partial class Form1 : Form
     {
+        private bool mouseDown;
+        private const int WM_NCLBUTTONDOWN = 0XA1;
+        private const int HT_CAPTION = 0x2;
+        private Timer timer;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool ReleaseCapture();
+
         public Form1()
         {
             InitializeComponent();
             DisplayInfo();
+
+            //timer 
+            timer = new Timer();
+            timer.Interval = 5000; // 5 sec
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width - this.Width, 0);
         }
 
         private void DisplayInfo()
         {
             // show current user
-            labelCurrentUser.Text = "Logged in as: " + Environment.UserName;
+            labelCurrentUser.Text = Environment.UserName;
 
             //show battery
             if (SystemInformation.PowerStatus.BatteryChargeStatus != BatteryChargeStatus.NoSystemBattery)
             {
                 int batteryPercent = (int)(SystemInformation.PowerStatus.BatteryLifePercent  * 100);
                 labelBatteryPercent.Text = "Battery: " + batteryPercent.ToString() + "%";
+
+                // form background based on batteryPercent
+                if (batteryPercent > 50)
+                    this.BackColor = Color.LightGreen;
+                else if (batteryPercent < 50 && batteryPercent > 25)
+                    this.BackColor = Color.Orange;
+                else
+                    this.BackColor = Color.Red;
+
+                //get remaining battery time
+                TimeSpan batteryLifeRemaining = TimeSpan.FromSeconds(SystemInformation.PowerStatus.BatteryLifeRemaining);
+                labelBatteryLifeRemaining.Text = "~" + batteryLifeRemaining.ToString(@"hh\:mm\:ss") + "est.";
             }
             else
             {
                 labelBatteryPercent.Text = "Battery: N/A";
+                labelBatteryLifeRemaining.Text = "No battery detected...";
             }
         }
 
-        //prevent user closing form
+        //refresh form
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DisplayInfo();
+        }
+
+        
+
+        //prevent closing maybe idk
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            e.Cancel = true;
-        }
-
-        //make form movable
-        private void Form1_MouseDown (object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
+            if (e.CloseReason == CloseReason.UserClosing)
             {
-                ReleaseCapture();
-                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+                e.Cancel = true;
             }
         }
 
-        private const int WM_NCLBUTTONDOWN = 0XA1;
-        private const int HT_CAPTION = 0x2;
+        //makes draggable
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
 
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        private static extern bool ReleaseCapture();
+            base.WndProc(ref m);
+        }
+
+        //stops altf4
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            switch (e.CloseReason)
+            {
+                case CloseReason.UserClosing:
+                    e.Cancel = true;
+                    break;
+            }
+
+            base.OnFormClosing(e);
+        }
+
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+          
+        }
+       
+
+        private void labelBatteryPercent_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelCurrentUser_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
